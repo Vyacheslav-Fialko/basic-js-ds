84ba9a5dd5b09fbf51bd529ddc61c7c357cda15e
VyacheslaviFialko <vyacheslav1@ukr.net>
2021-10-17 19:17:11 +0300

    feat: add solution stack test

--- src/st-stack.js
+++ src/st-stack.js
@@ -12,19 +12,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
- module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+module.exports = class Stack {
+  constructor() {
+    this.res = [];
+   }
+  push(element) {
+    this.res.push(element)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.res.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const lastIndex = this.res.length - 1;
+    return this.res[lastIndex]
   }
 }
